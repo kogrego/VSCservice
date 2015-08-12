094d14c3d79d6d82817c80097be0953ae0eed8ea
kogrego <kogrego@gmail.com>
2015-08-12 12:36:36 +0300

    created calsses, inteface and test class

---
+++ VCSService/GithubService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSservice 
+{
+    class GithubService : IVCSService
+    {
+        private static GithubService service;
+        private User user;
+        private Commit commit;
+        private Repo repo;
+
+        private GithubService()
+        {
+            
+        }
+        public static GithubService Service
+        {
+            get
+            {
+                if (service == null)
+                {
+                    service = new GithubService();
+                }
+                return service;
+            }
+        }
+        public string GetCommitInfo()
+        {
+            return "OK";
+        }
+        public string GetUserInfo()
+        {
+            return "OK";
+        }
+        public string GetRepoInfo()
+        {
+            return "OK";
+        }
+    }
+}
---
+++ VCSServiceTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace VCSServiceTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
